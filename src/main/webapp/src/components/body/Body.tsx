88f5df7603c4030c1c2657e2ed3984ff287168ee
gipobbe <56393691+gipobbe@users.noreply.github.com>
2021-12-11 15:16:15 +0100

    add page scaffold add header add footer todo: body added mui added icons material

---
+++ src/main/webapp/src/components/body/Body.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import '../../App.css';
+
+function Body() {
+  return (
+    <div className="appBody">
+      <div className="bodyContainer">
+        <h1>Body</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Body;
---
+++ src/main/webapp/src/components/header/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../../App.css';
+
+function Header() {
+  return(
+    <header>
+      <div className="headerContainer">
+        <h1>Todo App</h1>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
